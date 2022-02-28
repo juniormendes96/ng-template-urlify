3f36143dc243ec253e6137018b793b0641b54093
Junior <vilmarmjrr@gmail.com>
2022-02-28 14:13:34 -0300

    change test name

--- src/extract.spec.js
+++ src/extract.spec.js
@@ -2,7 +2,7 @@ const expect = require('expect.js');
 const extract = require('../src/extract');
 
 describe('extract', () => {
-  it('keeps the same content if no component is present', () => {
+  it('keeps the same content if no decorator is present', () => {
     const input = `
       export class AppComponent {
         constructor() {}
