7410dc8704413c450f6ac63c337531f7480d604c
Junior <vilmarmjrr@gmail.com>
2022-03-08 21:41:27 -0300

    fix cli variable reference

--- bin/cli.js
+++ bin/cli.js
@@ -29,7 +29,7 @@
     const templateUrl = `./${fileName.replace('.ts', '.html')}`;
     const result = extract(templateUrl, fileContent);
 
-    if (!result || !result.templateUrl?.trim()) {
+    if (!result || !result.templateHtml?.trim()) {
       return console.info(`No inline template found on ${fileName}`);
     }
 
